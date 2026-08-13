38c87c6d6b71ddf914f81e2721bb7da05143ef26
Deepen09-byte <deepen2918@gmail.com>
2026-08-13 16:32:51 +0530

    Register and Email verification done

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, verifyEmail, login } from "../controllers/auth.controller.js";
 import { registerValidation } from "../validators/auth.validator.js";
 
 const authRouter = Router()
 
 authRouter.post("/register", registerValidation, register)
+
+authRouter.post("/login",login)
+
+authRouter.get("/verify-email",verifyEmail)
 
 export default authRouter

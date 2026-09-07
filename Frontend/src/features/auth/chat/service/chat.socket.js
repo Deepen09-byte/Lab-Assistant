5ed165c97653bbdb32a620cfc51a6dc47415001b
Deepen09-byte <deepen2918@gmail.com>
2026-09-07 18:10:57 +0530

    Url updated

--- Frontend/src/features/auth/chat/service/chat.socket.js
+++ Frontend/src/features/auth/chat/service/chat.socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 export const initializeSocket= () => {
 
-    const socket = io("http://localhost:3000",{
+    const socket = io("https://lab-assistant-kivm.onrender.com",{
         withCredentials:true
     })
 

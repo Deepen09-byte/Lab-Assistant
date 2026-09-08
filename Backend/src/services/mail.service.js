c88b646e8cb37d9f439a693508294875fcda3b73
Deepen09-byte <deepen2918@gmail.com>
2026-09-08 16:37:07 +0530

    Brevo used

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -4,16 +4,13 @@ import dns from "dns"
 dns.setDefaultResultOrder("ipv4first") 
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
   family: 4,
   auth: {
-    type: 'OAuth2',
-    user: process.env.GOOGLE_USER,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    user: process.env.BREVO_SMTP_LOGIN,
+    pass: process.env.BREVO_SMTP_KEY,
   },
 });
 
